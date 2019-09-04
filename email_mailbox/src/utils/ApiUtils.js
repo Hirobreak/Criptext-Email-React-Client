7ccd0907d388c03627e7ab7446d2ae3b60ac919b
Pedro <pedro_aim@hotmail.com>
2019-09-04 14:56:08 -0500

    replacing get threads

--- email_mailbox/src/utils/ApiUtils.js
+++ email_mailbox/src/utils/ApiUtils.js
@@ -10,6 +10,8 @@ const apiBaseUrl =
     ? process.env.REACT_APP_DEV_API_URL
     : 'https://api.criptext.com';
 const aliceUrl = 'http://localhost';
+const debbyUrl = 'http://localhost:8086';
+
 // Default
 let osInfo = getOS();
 
@@ -134,6 +136,25 @@ export const generateKeyBundle = async ({
       recipientId,
       deviceId,
       accountId
+    })
+  };
+  return await fetch(requestUrl, options);
+};
+
+export const getThreads = async ({
+  accountId = 1,
+  limit = 22, 
+  date = Date.now().toString(),
+  labelId = 1
+}) => {
+  const requestUrl = `${debbyUrl}/threadsById`;
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      accountId,
+      limit, 
+      date,
+      labelId
     })
   };
   return await fetch(requestUrl, options);
