6b04843f0765a0cd1a6c442d595b26a53e8a1a3b
Pedro <pedro_aim@hotmail.com>
2019-09-20 15:47:15 -0500

    restarting alice

--- email_mailbox/src/utils/electronEventInterface.js
+++ email_mailbox/src/utils/electronEventInterface.js
@@ -700,7 +700,7 @@ const handleNewMessageEvent = async ({ rowid, params }) => {
           })
         : null;
     } catch (e) {
-      if (e.message === 'unavailable') {
+      if (e.message === signal.ALICE_ERROR) {
         return {
           rowId: null
         };
