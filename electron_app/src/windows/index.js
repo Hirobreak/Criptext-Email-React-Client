43236b7bd81baabd5b0f1d0d7af31aa21809cdfc
erikaperugachi <erikaperugachi@gmail.com>
2019-10-02 12:12:34 -0500

    Add step 4: confirm pin

--- electron_app/src/windows/index.js
+++ electron_app/src/windows/index.js
@@ -1,5 +1,17 @@
+const keytar = require('keytar');
 const dbManager = require('./../database');
+const myAccount = require('./../Account');
+const mySettings = require('./../Settings');
+const loginWindow = require('./login');
+const mailboxWindow = require('./mailbox');
 const pinWindow = require('./pin');
+const { EVENTS, addEvent } = require('./events');
+const { createAppMenu } = require('./menu');
+const wsClient = require('./../socketClient');
+const { initClient } = require('./../clientManager');
+const { initNucleus } = require('./../nucleusManager');
+const globalManager = require('./../globalManager');
+const { isFromStore, getSystemLanguage } = require('./windowUtils');
 
 const upStepDBEncryptedWithoutPIN = async () => {
   const [existingAccount] = await dbManager.getAccount();
@@ -8,7 +20,85 @@ const upStepDBEncryptedWithoutPIN = async () => {
   }
 };
 
+const upStepCheckPINDBEncrypted = async () => {
+  const pin = await pinWindow.checkPin();
+  if (!pin) {
+    globalManager.pinData.set({ pinType: 'new' });
+    pinWindow.show();
+    return;
+  }
+
+  try {
+    await dbManager.initDatabaseEncrypted({ key: pin });
+  } catch (error) {
+    globalManager.pinData.set({ pinType: 'new' });
+    pinWindow.show();
+    return;
+  }
+
+  await upApp({ pin });
+};
+
+const upApp = async ({ shouldSave, pin }) => {
+  if (pin) {
+    if (shouldSave) {
+      keytar
+        .setPassword('CriptextMailDesktopApp', 'unique', `${pin}`)
+        .then(result => {
+          console.log('result', result);
+        })
+        .catch(error => {
+          console.log('error', error);
+        });
+    }
+    globalManager.databaseKey.set(pin);
+  }
+
+  const [existingAccount] = await dbManager.getAccount();
+  if (!existingAccount) {
+    const language = await getUserLanguage();
+    await initClient();
+    initNucleus({ language });
+    createAppMenu();
+    loginWindow.show({});
+    return;
+  }
+
+  if (existingAccount.deviceId) {
+    await upMailboxWindow(existingAccount);
+  } else {
+    const language = await getUserLanguage();
+    await initClient();
+    const settings = { isFromStore, language };
+    mySettings.initialize(settings);
+    initNucleus({ language });
+    createAppMenu();
+    loginWindow.show({});
+  }
+};
+
+const upMailboxWindow = async existingAccount => {
+  const appSettings = await dbManager.getSettings();
+  const settings = Object.assign(appSettings, { isFromStore });
+  myAccount.initialize(existingAccount);
+  mySettings.initialize(settings);
+  await initClient();
+  initNucleus({ language: mySettings.language });
+  wsClient.start(myAccount);
+  createAppMenu();
+  mailboxWindow.show({ firstOpenApp: true });
+  if (pinWindow) pinWindow.close({ forceClose: true });
+};
+
+const getUserLanguage = async () => {
+  return await getSystemLanguage();
+};
+
+addEvent(EVENTS.Up_app, upApp);
+
 module.exports = {
   dbManager,
-  upStepDBEncryptedWithoutPIN
+  upApp,
+  upStepDBEncryptedWithoutPIN,
+  upStepCheckPINDBEncrypted
 };
