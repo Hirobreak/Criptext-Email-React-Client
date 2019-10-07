9ac82973abcfb44c5212a7d00d3c53e2ce49f734
erikaperugachi <erikaperugachi@gmail.com>
2019-10-07 16:46:24 -0500

    Sequelize: Add Account querys and test

--- electron_app/src/database/DBEmanager.js
+++ electron_app/src/database/DBEmanager.js
@@ -7,20 +7,59 @@ const {
   Feeditem,
   File,
   Label,
+  deleteDatabase,
   getDB,
   initDatabaseEncrypted,
   resetKeyDatabase,
+  Op,
   Table
 } = require('./DBEmodel.js');
+const { noNulls } = require('../utils/ObjectUtils');
 
-const getAccount = () => {
-  const db = getDB();
-  if (!db) return [];
+/* Account
+----------------------------- */
+const createAccount = async params => {
+  return await Account().create(params);
 };
 
-const getAccountByParams = () => {
-  const db = getDB();
-  if (!db) return [];
+const getAccount = async () => {
+  return await Account().findAll();
+};
+
+const getAccountByParams = async params => {
+  return await Account().findAll({ where: params });
+};
+
+const updateAccount = async ({
+  deviceId,
+  jwt,
+  refreshToken,
+  name,
+  privKey,
+  pubKey,
+  recipientId,
+  registrationId,
+  signature,
+  signatureEnabled,
+  signFooter
+}) => {
+  const params = noNulls({
+    deviceId,
+    jwt,
+    refreshToken,
+    name,
+    privKey,
+    pubKey,
+    registrationId,
+    signature,
+    signatureEnabled:
+      typeof signatureEnabled === 'boolean' ? signatureEnabled : undefined,
+    signFooter: typeof signFooter === 'boolean' ? signFooter : undefined
+  });
+
+  return await Account().update(params, {
+    where: { recipientId: { [Op.eq]: recipientId } }
+  });
 };
 
 module.exports = {
@@ -33,9 +72,12 @@ module.exports = {
   File,
   Label,
   Table,
+  createAccount,
+  deleteDatabase,
   getDB,
   getAccount,
   getAccountByParams,
   initDatabaseEncrypted,
-  resetKeyDatabase
+  resetKeyDatabase,
+  updateAccount
 };
