abda314d4620f07f5fe0c76d63bd88e51b4db621
erikaperugachi <erikaperugachi@gmail.com>
2019-10-08 01:01:56 -0500

    Sequelize: Add Contact querys and test

--- electron_app/src/database/DBEmanager.js
+++ electron_app/src/database/DBEmanager.js
@@ -21,6 +21,7 @@ const {
   Table
 } = require('./DBEmodel.js');
 const { noNulls } = require('../utils/ObjectUtils');
+const { formContactsRow } = require('../utils/dataTableUtils.js');
 
 /* Account
 ----------------------------- */
@@ -70,6 +71,98 @@ const updateAccount = async ({
   });
 };
 
+/* Contact
+----------------------------- */
+const createContact = async params => {
+  return await Contact().bulkCreate(params);
+};
+
+const createContactsIfOrNotStore = async (contacts, trx) => {
+  const parsedContacts = filterUniqueContacts(formContactsRow(contacts));
+  const contactsMap = parsedContacts.reduce((contactsObj, contact) => {
+    contactsObj[contact.email] = contact;
+    return contactsObj;
+  }, {});
+  const emailAddresses = Object.keys(contactsMap);
+  const contactsFound = await Contact().findAll({
+    where: { email: emailAddresses },
+    transaction: trx
+  });
+  const contactsToUpdate = contactsFound.reduce((toUpdateArray, contact) => {
+    const email = contact.email;
+    const newName = contactsMap[email].name || contact.name;
+    if (newName !== contact.name) {
+      toUpdateArray.push({ email, name: newName });
+    }
+    return toUpdateArray;
+  }, []);
+
+  const storedEmailAddresses = contactsFound.map(
+    storedContact => storedContact.email
+  );
+  const newContacts = parsedContacts.filter(
+    contact => !storedEmailAddresses.includes(contact.email)
+  );
+
+  if (newContacts.length) {
+    await Contact().bulkCreate(newContacts, { transaction: trx });
+  }
+  if (contactsToUpdate.length) {
+    await Promise.all(
+      contactsToUpdate.map(contact => updateContactByEmail(contact, trx))
+    );
+  }
+  return emailAddresses;
+};
+
+const getAllContacts = async () => {
+  return await Contact().findAll({
+    attributes: ['name', 'email'],
+    order: [['score', 'DESC'], ['name']]
+  });
+};
+
+const getContactByEmails = async (emails, trx) => {
+  return await Contact().findAll({
+    attributes: ['id', 'email', 'score', 'spamScore'],
+    where: { email: emails },
+    transaction: trx
+  });
+};
+
+const getContactByIds = async (ids, trx) => {
+  return await Contact().findAll({
+    attributes: ['id', 'email', 'name'],
+    where: { id: ids },
+    raw: true,
+    transaction: trx
+  });
+};
+
+const updateContactByEmail = async ({ email, name }, trx) => {
+  return await Contact().update(
+    { name },
+    { where: { email: { [Op.eq]: email } }, transaction: trx }
+  );
+};
+
+/* Functions
+----------------------------- */
+const filterUniqueContacts = contacts => {
+  const contactsUnique = contacts.reduce(
+    (result, contact) => {
+      const obj = Object.assign(result);
+      if (!result.stack[contact.email]) {
+        obj.stack[contact.email] = contact;
+        obj.contacts.push(contact);
+      }
+      return obj;
+    },
+    { stack: {}, contacts: [] }
+  );
+  return contactsUnique.contacts;
+};
+
 module.exports = {
   Account,
   Contact,
@@ -87,11 +180,17 @@ module.exports = {
   Signedprekeyrecord,
   Table,
   createAccount,
+  createContact,
+  createContactsIfOrNotStore,
   deleteDatabase,
   getDB,
   getAccount,
   getAccountByParams,
+  getAllContacts,
+  getContactByEmails,
+  getContactByIds,
   initDatabaseEncrypted,
   resetKeyDatabase,
-  updateAccount
+  updateAccount,
+  updateContactByEmail
 };
